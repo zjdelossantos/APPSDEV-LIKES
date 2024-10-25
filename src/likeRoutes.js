901552ba56d222082219afa297ee378f12763021
Zeldrickjesus <143236106+zjdelossantos@users.noreply.github.com>
2024-10-25 16:11:21 +0930

    Update likeRoutes.js

--- src/likeRoutes.js
+++ src/likeRoutes.js
@@ -1,11 +1,12 @@
 import express from 'express';
-import { addLike, removeLike, getAllLikes, hasLiked } from '../controllers/likeController.js';
+import { createLike, getLikes, getLikeById, updateLike, deleteLike } from '../controllers/likeController';
 
 const router = express.Router();
 
-router.post('/like', addLike);
-router.delete('/like', removeLike);
-router.get('/likes/:thread_id', getAllLikes);
-router.get('/like-status/:thread_id/:account_id', hasLiked);
+router.post('/likes', createLike);
+router.get('/likes', getLikes);
+router.get('/likes/:id', getLikeById);
+router.put('/likes/:id', updateLike);
+router.delete('/likes/:id', deleteLike);
 
 export default router;
